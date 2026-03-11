74082dbe99b93363a096259aa08204ae611fbbc3
Meow <30401521+me0wg4ming@users.noreply.github.com>
2026-03-11 06:24:55 +0100

    showing again how many frames we unregistered

--- __Anticrash.lua
+++ __Anticrash.lua
@@ -1,3 +1,5 @@
+local PREFIX = "|cffff0000__|r|cffaa00ffAnticrash|r"
+
 local f = CreateFrame("Frame")
 f:RegisterEvent("PLAYER_LOGOUT")
 f:SetScript("OnEvent", function()
@@ -8,11 +10,14 @@ end)
 f:SetScript("OnUpdate", function()
   if not f.shutdownPending then return end
   f.shutdownPending = false
+  local count = 0
   local frame = EnumerateFrames()
   while frame do
     if frame.UnregisterAllEvents then
       frame:UnregisterAllEvents()
+      count = count + 1
     end
     frame = EnumerateFrames(frame)
   end
+  DEFAULT_CHAT_FRAME:AddMessage(PREFIX .. " |cffff0000UNREGISTERED " .. count .. " FRAMES BEFORE LOGOUT|r")
 end)
